fa74159610047f358b6fed99322ec6921c9234b1
jia-guo <antipasjiajia@gmail.com>
2021-07-22 22:47:56 -0700

    delete sth

--- utils/utils/lib/utils.js
+++ utils/utils/lib/utils.js
@@ -7,7 +7,6 @@ function isObject(o) {
 function sleep(ms = 1000) {
   return new Promise((resolve) => setTimeout(resolve, ms));
 }
-
 async function sleepAsync(ms = 1000) {
   return await new Promise((resolve) => setTimeout(resolve, ms));
 }
